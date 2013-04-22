bfa74b0b68d0ae2754dc986fa53a7fae49e10c0e
Nicholas McCurry <nick.mc.ski1@gmail.com>
2013-04-22 13:10:19 -0600

    Create config.php
    Configuration for the site

---
+++ config.php
@@ -0,0 +1,35 @@
+<?
+//Basic config options
+$sitename="McModding";
+
+//MySQL Domain Info
+$db_host = "localhost";
+$db = "**********************";
+$db_user = "*****************";
+$db_pass = "*****************";
+
+//=================Permissions=================
+//Home page
+$lvl_5_home = "user_management.php";
+$lvl_4_home = "stream_management.php";
+$lvl_3_home = "moderator.php";
+$lvl_2_home = "1482.php";
+$lvl_1_home = "test.php";
+$banned_home = "banned.php";
+$failed_login_page = "failed.php";
+
+//Name alisias
+$lvl_5_name = "Admin";
+$lvl_4_name = "Stream Manager";
+$lvl_3_name = "Moderator";
+$lvl_2_name = "Team 1482";
+$lvl_1_name = "Default";
+
+//=================Login=================
+
+$cookie_name_user = "modding_user";
+$cookie_name_key = "mod_Key";
+$cookie_name_last = "lastUsername";
+$default_permission = "1";
+$login_page = "login.php";
+?>
