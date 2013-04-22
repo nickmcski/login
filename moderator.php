63d00daff8fc2a53a4b30494f097bb4c929f22b6
Nicholas McCurry <nick.mc.ski1@gmail.com>
2013-04-22 13:19:51 -0600

    Create moderator.php
    A secure page that only users with lvl3 permissions can access

---
+++ moderator.php
@@ -0,0 +1,45 @@
+<?
+$req_permission = '3';
+include"auth.php";
+include"functions.php";
+?>
+<html>
+<head>
+<?
+head("../..", "../css");
+?>
+
+</head>
+
+<body>
+<center>
+<p id="Title">Moderator pages</p>
+</center>
+<?
+include"navbar.php";
+?>
+<div id="Header">
+Update
+</div>
+<div align="left" id="content">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae leo id erat lacinia auctor. Proin sodales commodo cursus. Praesent porttitor varius felis tempor iaculis. Aenean aliquam ullamcorper justo sed molestie. Donec massa nisl, eleifend non consequat a, ornare vel eros. Nam nisl augue, rutrum vitae volutpat vitae, pretium eu neque. Sed sed tellus odio, vitae pretium massa. Aenean malesuada sapien non nisi aliquet feugiat. Fusce viverra mi at dui vehicula vestibulum. Suspendisse pellentesque dictum est, a molestie metus tempus a.
+</p>
+<p>
+Duis tempus fermentum tortor, a commodo justo cursus quis. Cras interdum, ipsum in faucibus convallis, odio risus porttitor arcu, eu tincidunt leo libero ac odio. Nam eu tortor vitae dolor accumsan condimentum eu nec odio. Aliquam lacus nunc, faucibus consequat vestibulum eu, imperdiet eget metus. Mauris sit amet justo vitae nisl porta consequat. Ut gravida ligula nec tellus lacinia vel eleifend metus laoreet. Mauris tempus massa vel tortor elementum sagittis. Nam sollicitudin est libero, vitae ultricies lectus.
+</p>
+<p>
+Morbi dignissim aliquet libero. Nullam consectetur pulvinar aliquam. Aliquam adipiscing imperdiet faucibus. Etiam mattis auctor risus et semper. Quisque scelerisque enim eget nibh ullamcorper sagittis. Vestibulum hendrerit vestibulum ligula, ut facilisis dui laoreet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam suscipit rhoncus elit quis hendrerit. Integer mattis, turpis mollis aliquam pharetra, diam velit malesuada ligula, et venenatis lectus metus vitae lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer cursus nulla nec tellus pellentesque at posuere nisl placerat. Ut quis sem est, vitae imperdiet arcu. Maecenas nec eleifend leo.
+</p>
+<p>
+Aliquam adipiscing eros urna, quis mattis mauris. Suspendisse et est vel lorem pellentesque feugiat. Duis vitae pulvinar nisi. Mauris luctus odio eu justo viverra posuere. Nulla quis turpis neque, nec euismod eros. Ut nunc turpis, auctor at imperdiet at, facilisis eu erat. Fusce iaculis auctor lorem, in ornare mi hendrerit et. Phasellus augue sapien, vestibulum sed aliquet in, suscipit a ipsum.
+</p>
+<p>
+Aliquam non neque lacus, id dapibus eros. Suspendisse sed nibh pretium elit aliquet venenatis. Nulla eros lacus, mollis sit amet faucibus sit amet, scelerisque vel felis. Phasellus posuere velit quis lectus lobortis gravida. Vestibulum orci libero, lobortis nec facilisis at, dapibus nec lectus. Nam ullamcorper, lorem in porta aliquet, est elit consectetur urna, ut tempor sem ante id purus. Nulla sed erat urna. Phasellus rutrum leo vitae neque aliquam sed accumsan tortor accumsan. Duis non ipsum odio. Suspendisse potenti. Curabitur et tellus et urna dictum molestie. Suspendisse potenti. Nunc nec orci eget magna sollicitudin euismod. Donec mattis mi sit amet leo faucibus dictum.
+</p>
+</div>
+
+
+
+</body>
+</html>
