060c1204f5675a69ca0e632f29ff59a3ee81bd7c
Nicholas McCurry <nick.mc.ski1@gmail.com>
2013-04-23 12:56:35 -0600

    Commented file

--- auth.php
+++ auth.php
@@ -27,17 +27,17 @@ if($found != 1 &&$req_permission != ''){
 	exit;
 
 }else{
-
-  //Get User info
+//-------Get user info-------
   
+  //If user cookie is valid
   if($userCookie != NULL){
   
   	$query = "SELECT * FROM users WHERE `username`='$userCookie'";
 	$result = mysql_query($query) or die("Couldn't execute query");
 	$count = mysql_num_rows($result);  
-	
+	//If users are returned with that username
 	if($count == 1){
-  
+  		//Get info on that user
 		while ($row= mysql_fetch_array($result)) {
 			$permissions= $row["permissions"];
 			$first_name= $row["firstname"];
@@ -49,8 +49,9 @@ if($found != 1 &&$req_permission != ''){
 		}
 		//doubble check they have permission
 		if($permissions > $var1){
-		//Look if key matches
+			//Look if key matches
 			if($key != $cookey&&$key != NULL){
+				//Key is invalid, remove all keys and redirect to failed login page
 		  		header("location:$failed_login_page");
 		  		setcookie($cookie_name_user, $cookie_value_user, time()-18000, "/", "mcmodding.org");
 				setcookie($cookie_name_key, $cookie_value_key, time()-18000, "/", "mcmodding.org");
@@ -69,34 +70,39 @@ if($found != 1 &&$req_permission != ''){
 		  
 	  }else{
 	  	//No data for the user
+	  	//redirect to login page
 		  header("location:login.php");
 		  exit;
 	  }
 	  //print username from cookie
 	  $username = $userCookie;
-
+	//Usercookie is not found
+	//Useing last username
 	}elseif($lastUsername != NULL){
 	
 		$query = "SELECT * FROM users WHERE `username`='$lastUsername'";
 		$result = mysql_query($query) or die("Couldn't execute query");
 		$count = mysql_num_rows($result);  
-		
+		//Get users returned
 		if($count == 1){
 			while ($row= mysql_fetch_array($result)) {
 				$username= $lastUsername;
+				//Set permissions to zero since user is not logged in
 				$permissions= 0;
+				//Get remaining info on user
 				$first_name= $row["firstname"];
 				$last_name= $row["lastname"];
 				$mc_name= $row["minecraftname"];
 				$banned = $row["BANNED"];
 			}
+			//redirect to banned page if banned 
 			if($banned == 1){
 			  	header("location:$banned_home");
 			  	exit;
 		  	}
 		}
 	}else{
-	//User cookie not found, and page does not require permission to view
+	//User cookie not found and last login not found.
 	$username = "Guest";
 	$permissions = '0';
 
